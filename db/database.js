8e22080ee5c0a90cd4c735e1a62a05b628c6635c
JorgeDev <espinozadiazjorgeluis@gmail.com>
2023-09-04 20:09:41 -0500

    update

--- db/database.js
+++ db/database.js
@@ -1,7 +1,16 @@
+require("dotenv").config();
+
 const { Sequelize } = require("sequelize");
-const db = new Sequelize("jorge", "root", "jorge@@2023", {
-  host: "localhost",
-  dialect: "mysql",
-});
+
+const db = new Sequelize(
+  process.env.DATABASE,
+  process.env.USERNAMEssss,
+  process.env.PASSWORD, // Corrige el nombre de la variable a PASSWORD
+  {
+    host: process.env.HOST, // Utiliza variables de entorno para host y dialect también si es necesario
+    dialect: process.env.DIALECT,
+  }
+);
+console.log(process.env.PASSWORD);
 
 module.exports = db;
