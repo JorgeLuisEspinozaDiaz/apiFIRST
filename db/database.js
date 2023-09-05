fdee74b65d27deb6cc27ea5f41c764d6c62b0ce1
JorgeDev <espinozadiazjorgeluis@gmail.com>
2023-09-04 20:39:26 -0500

    chage

--- db/database.js
+++ db/database.js
@@ -7,6 +7,7 @@ const db = new Sequelize(
   process.env.USERNAMEssss,
   process.env.PASSWORD, // Corrige el nombre de la variable a PASSWORD
   {
+    port: process.env.PORTS,
     host: process.env.HOST, // Utiliza variables de entorno para host y dialect también si es necesario
     dialect: process.env.DIALECT,
   }
@@ -16,5 +17,6 @@ console.log(process.env.HOST);
 console.log(process.env.DIALECT);
 console.log(process.env.USERNAMEssss);
 console.log(process.env.PASSWORD);
+console.log(process.env.PORTS);
 
 module.exports = db;
